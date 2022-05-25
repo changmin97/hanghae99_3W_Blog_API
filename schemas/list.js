697ac525f6766ad7b2ccf5d5f4d302a1e90a0994
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-05-26 04:23:35 +0900

    first commit

---
+++ schemas/list.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const listSchema = new mongoose.Schema({
+    listId: {
+        type: Number,
+        require: true,
+        unique: true,
+    },
+    title: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    content: {
+        type: String,
+    },
+    password: {
+        type: Number,
+        required: true,
+    },
+    createAt: {
+        type: Date
+      }
+})
+
+module.exports = mongoose.model("List", listSchema )
+
+// db.lists.insert(
+// {
+//         "listId" : 4,
+//         "title": "아자아자",
+//         "name": "이창민",
+//         "content": "로론애ㅔㅑ러",
+//         "password": 331   
+//     }
